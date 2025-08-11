1774b4a57310c3463829c23f94a41df5a46534c2
KimGiii <gibo9809@gmail.com>
2025-08-11 15:50:58 +0900

    feat: 스웨거 설정 및 Spring security 연동

---
+++ src/main/java/shoppingmall/hanaro/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package shoppingmall.hanaro.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Hanaro Shopping Mall API")
+                .version("v1.0.0")
+                .description("하나로 쇼핑몰 프로젝트 API 명세서입니다.");
+
+        // Security 스키마 설정
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(java.util.List.of(securityRequirement))
+                .info(info);
+    }
+}
